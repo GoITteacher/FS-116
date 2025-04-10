ce5b1a35b3784ff0c3439d6a2b16a9b1b10cd8c8
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-04-10 22:07:11 +0300

    update

--- js/01-class.js
+++ js/01-class.js
@@ -8,3 +8,185 @@
  *  - Геттери і сеттери
  *  - Статичні властивості
  */
+
+// const user1 = {
+//   firstname: 'Vasya',
+//   lastName: 'Test',
+//   age: 25,
+
+// };
+// const user2 = {
+//   firstname: 'Vasya',
+//   lastName: 'Test',
+//   age: 25,
+// };
+// const user3 = {
+//   firstname: 'Vasya',
+//   lastName: 'Test',
+//   age: 25,
+// };
+// const user4 = {
+//   firstname: 'Vasya',
+//   lastName: 'Test',
+//   age: 25,
+// };
+// const user5 = {
+//   firstname: 'Vasya',
+//   lastName: 'Test',
+//   age: 25,
+// };
+
+// class User {
+
+//   constructor(userFirstName, userLastName, userAge) {
+//     this.firstname = userFirstName;
+//     this.age = userAge;
+//     this.lastName = userLastName;
+//     this.balance = 0;
+//   }
+
+//   showName() {
+//     console.log(this.firstname);
+//   }
+
+//   showAge() {
+//     console.log(this.age);
+//   }
+
+//   showInfo() {
+//     this.showName();
+//     this.showAge();
+//   }
+// }
+
+// const user1 = new User('Volodymyr', 'Pashchenko', 23);
+
+// !======================================================
+
+// class Student {
+//   #balance;
+
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//     this.#balance = 0;
+//   }
+
+//   #showName() {
+//     console.log('\n\n', this.name);
+//   }
+
+//   #showAge() {
+//     console.log(this.age);
+//   }
+
+//   showInfo() {
+//     this.#showName();
+//     this.#showAge();
+//     console.log('Balance:', this.#balance);
+//   }
+
+//   getStudentBalance() {
+//     return this.#balance;
+//   }
+
+//   setStudentBalance(newBalance) {
+//     if (newBalance < 1000) {
+//       this.#balance = newBalance;
+//     }
+//   }
+// }
+
+// const student1 = new Student('Roman', 22);
+
+// student1.showInfo();
+
+// student1.balance = 9999999;
+
+// student1.showInfo();
+
+// student1.setStudentBalance(9999999);
+// student1.setStudentBalance(500);
+
+// student1.showInfo();
+
+//!======================================================
+
+// class Student {
+//   #balance;
+
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//     this.#balance = 0;
+//   }
+
+//   set balance(newBalance) {
+//     if (newBalance >= 0) {
+//       this.#balance = newBalance;
+//     }
+//   }
+
+//   get test() {
+//     console.log('Hello test');
+//   }
+// }
+
+// const student1 = new Student('Roman', 22);
+
+// student1.balance = 35;
+
+// console.log(student1.balance);
+
+// student1.balance = 25;
+
+// console.log(student1.balance);
+
+// console.log(student1);
+
+//!======================================================
+
+// class Product {
+//   static discount = 10;
+//   static storeName = 'Halava';
+//   static amount = 0;
+
+//   constructor(title, price, amount) {
+//     this.title = title;
+//     this.price = price;
+//     this.amount = amount;
+
+//     Product.amount += amount;
+//   }
+
+//   showInfo() {
+//     const info = `=====================
+// Title: ${this.title}
+// Price: ${this.price * Product.discount}
+// Amount: ${this.amount}
+// Total: ${this.price * Product.discount * this.amount}
+// =====================\n`;
+
+//     console.log(info);
+//   }
+
+//   static showTotalProductAmoun() {
+//     console.log('Amount:', Product.amount);
+//   }
+// }
+
+// const product1 = new Product('Iphone', 1500, 2);
+// const product2 = new Product('MacBook', 3000, 5);
+// const product3 = new Product('Audi', 20000, 1);
+
+// console.log(Product.amount);
+
+//!======================================================
+
+// Product.discount = 0.9;
+
+// product1.showInfo();
+
+// Product.showTotalProductAmoun();
+
+// product1.showInfo();
--- js/02-inheritance.js
+++ js/02-inheritance.js
@@ -5,3 +5,61 @@
  *  - Конструктор дочірнього класу (super)
  *  - Методи дочірнього класу
  */
+
+// class Animal {
+//   constructor(breed, age, weight) {
+//     this.breed = breed;
+//     this.age = age;
+//     this.weight = weight;
+//   }
+
+//   sleep() {
+//     console.log('zzzz');
+//   }
+
+//   eat() {
+//     console.log('...');
+//   }
+// }
+
+// class Dog extends Animal {
+//   constructor(name, breed, age, weight, height) {
+//     super(breed, age, weight);
+//     this.name = name;
+//     this.height = height;
+//   }
+
+//   sayWoof() {
+//     console.log('Woof woof');
+//   }
+
+//   eat() {
+//     console.log('Dog eat');
+//   }
+// }
+
+// const dog1 = new Dog('Lord', 'Spaniel', 5, 15, 50);
+
+// console.log(dog1);
+// dog1.eat();
+//!======================================================
+
+// class Cat {
+//   constructor(name, breed, age, weight, height) {
+//     this.name = name;
+//     this.height = height;
+//   }
+
+//   sayMeow() {
+//     console.log('meow meow');
+//   }
+// }
+
+// class Bird {
+//   constructor(breed, age, weight, height) {}
+
+//   fly() {
+//     console.log('fly');
+//   }
+// }
+//!======================================================
